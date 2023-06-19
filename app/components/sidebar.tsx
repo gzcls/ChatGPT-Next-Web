756974e0f5c39b7919eb5e8bbade407131ad1e27
gzcls <136059643+gzcls@users.noreply.github.com>
2023-06-19 17:18:33 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -122,6 +122,7 @@ export function SideBar(props: { className?: string }) {
         <div className={styles["sidebar-title"]}>AI助手</div>
         <div className={styles["sidebar-sub-title"]}>
           您的个人专属助手
+          （请不要录入个人或公司的敏感信息）
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
